6103015f33ee281b5a15d99a3a4603aecec9cc31
developersrinu <orsusrinu606@gmail.com>
2024-02-18 17:27:28 +0530

    ok

--- frontend/src/SocketContext.js
+++ frontend/src/SocketContext.js
@@ -230,13 +230,99 @@
 
 
 
+// import React, { useState, createContext, useRef, useEffect } from "react";
+// import { io } from 'socket.io-client';
+// import Peer from 'simple-peer';
+
+// const SocketContext = createContext();
+
+// const socket = io('https://vedio-chat.onrender.com/');
+
+// const ContextProvider = ({ children }) => {
+//     const [stream, setStream] = useState(null);
+//     const [me, setMe] = useState('');
+//     const [call, setCall] = useState({});
+//     const [callAccepted, setCallAccepted] = useState(false);
+//     const [callEnded, setCallEnded] = useState(false);
+//     const [name, setName] = useState('');
+//     const myVideo = useRef();
+//     const userVideo = useRef();
+//     const connectionRef = useRef();
+
+//     useEffect(() => {
+//         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+//             .then((currentStream) => {
+//                 setStream(currentStream);
+//                 console.log(currentStream)
+//                 myVideo.current.srcObject = currentStream;
+//               console.log(myVideo.current.srcObject)
+
+//             });
+
+//         socket.on('me', (id) => {
+//             setMe(id);
+//         });
+
+//         socket.on('calluser', ({ from, name: callerName, signal }) => {
+//             setCall({ isReceivedCall: true, from, name: callerName, signal });
+//         });
+//     }, []);
+
+//     const answerCall = () => {
+//         setCallAccepted(true);
+//         const peer = new Peer({ initiator: false, trickle: false, stream });
+//         peer.on('signal', (data) => {
+//             socket.emit('answercall', { signal: data, to: call.from });
+//         });
+//         peer.on('stream', (currentStream) => {
+//             userVideo.current.srcObject = currentStream;
+//         });
+//         peer.signal(call.signal);
+//         connectionRef.current = peer;
+//     };
+
+//     const callUser = (id) => {
+//         const peer = new Peer({ initiator: true, trickle: false, stream });
+//         peer.on('signal', (data) => {
+//             socket.emit('calluser', { userToCall: id, signalData: data, from: me, name });
+//         });
+//         peer.on('stream', (currentStream) => {
+//             userVideo.current.srcObject = currentStream;
+//         });
+
+//         socket.on('callaccepted', (signal) => {
+//             setCallAccepted(true);
+//             peer.signal(signal);
+//             connectionRef.current = peer;
+//         });
+//     };
+
+//     const leaveCall = () => {
+//         setCallEnded(true);
+//         if (connectionRef.current) {
+//             connectionRef.current.destroy();
+//         }
+//         window.location.reload(); // Reload the page after leaving the call
+//     };
+
+//     return (
+//         <SocketContext.Provider value={{ stream, me, myVideo, userVideo, answerCall, call, callAccepted, callEnded, callUser, leaveCall, setName }}>
+//             {children}
+//         </SocketContext.Provider>
+//     );
+// }
+
+// export { ContextProvider, SocketContext };
+
+
+
 import React, { useState, createContext, useRef, useEffect } from "react";
 import { io } from 'socket.io-client';
 import Peer from 'simple-peer';
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io('https://vedio-chat.onrender.com/');
 
 const ContextProvider = ({ children }) => {
     const [stream, setStream] = useState(null);
@@ -253,11 +339,7 @@ const ContextProvider = ({ children }) => {
         navigator.mediaDevices.getUserMedia({ video: true, audio: true })
             .then((currentStream) => {
                 setStream(currentStream);
-                console.log(currentStream)
-
-              const aa =  myVideo.current.srcObject = currentStream;
-              console.log(aa)
-
+                myVideo.current.srcObject = currentStream;
             });
 
         socket.on('me', (id) => {
